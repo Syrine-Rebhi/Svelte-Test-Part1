3c9b40f090eb42eaa80c5b2a92cfea34843e4502
Syrine-Rebhi <90925704+Syrine-Rebhi@users.noreply.github.com>
2022-04-28 17:42:28 +0100

    Create Menu.svelte

---
+++ Menu.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let menu;
+	export let selected ;
+	import AddMenuItem from "./AddMenuItem.svelte"
+	
+	import { createEventDispatcher } from 'svelte';
+  let dispatch = createEventDispatcher();
+	
+</script>
+
+<div class="menu">
+	{#each menu as item}
+		<div class="menu-item" class:selected={selected == item.title} on:click={() => dispatch('selectedMenuItemChange', item)}>
+			{item.title}
+		</div>
+	{/each}
+	
+	<button on:click={() => dispatch('onAddButtonClicked')}>
+		Add New Menu Item
+	</button>
+</div>
+
+<style>
+	.menu {
+		margin-top: 30px;
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+	}
+	
+	.menu .menu-item {
+		padding: 10px 6px;
+		margin: 0 5px;
+		font-size: 16px;
+		cursor: pointer;
+	}
+	
+	.menu .menu-item.selected {
+		border-bottom: 4px solid #000;
+	}
+</style>
