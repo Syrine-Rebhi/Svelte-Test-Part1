5a4c300286883ca5a05f576c0fb5f4507b4c4b32
Syrine-Rebhi <90925704+Syrine-Rebhi@users.noreply.github.com>
2022-04-28 17:41:29 +0100

    Create AddMenuItem.svelte

---
+++ AddMenuItem.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+  let dispatch = createEventDispatcher();
+	let menuItemTitle = '';
+	let menuItemContent = '';	
+	
+  function handleAddNewMenuItem () {
+	  const formValues = {title: menuItemTitle,content: menuItemContent};
+		dispatch('submitItem',formValues);
+	}
+
+</script>
+
+<div>
+	<input type="text" placeholder="Add new menu item" bind:value={menuItemTitle}>	
+	<textarea placeholder="text to show when we click on the new item" bind:value={menuItemContent}></textarea>
+	<button on:click={handleAddNewMenuItem}>
+		Add menu item
+	</button>
+</div>
+
+<style>
+	input, textarea {
+		margin-top: 15px;
+		padding: 10px;
+		border: 1px solid #777;
+		width: 100%;
+	}
+</style>
